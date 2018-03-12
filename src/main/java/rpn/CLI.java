1a2af68724bbf4e1504a32a66678575aa3c1aed1
vbonassi <vbonassi@cisco.com>
2018-03-12 14:24:40 +0100

    add rpn code and tests

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -1,18 +1,51 @@
 package rpn;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.*;
+import java.util.*;
 
 public class CLI {
-    public static final void main(String[] args) {
-        String expression = Stream.of(args).collect(Collectors.joining(" "));
-
-        System.out.println("About to evaluate '" + expression + "'");
-        long result = evaluate(expression);
-        System.out.println("> " + result);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            String s = in.readLine();
+            if (s==null) break;
+            Stack<String> tks = new Stack<String>();
+            tks.addAll(Arrays.asList(s.trim().split("[ \t]+")));
+            if (tks.peek().equals("")) continue;
+            try {
+                double r = evaluate(tks);
+                if (!tks.empty()) throw new Exception();
+                System.out.println(r);
+            }
+            catch (Exception e) {System.out.println("error");}
+        }
     }
 
-    static long evaluate(String expression) {
-        return 0;
+    static double evaluate(String string) {
+        try {
+            Stack<String> stack = new Stack<>();
+            String [] strings = string.split(" ");
+            for (String s : strings) {
+                stack.push(s);
+            }
+            return evaluate(stack);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    private static double evaluate(Stack<String> tks) throws Exception {
+        String tk = tks.pop();
+        double x,y;
+        try {x = Double.parseDouble(tk);}
+        catch (Exception e) {
+            y = evaluate(tks);  x = evaluate(tks);
+            if      (tk.equals("+"))  x += y;
+            else if (tk.equals("-"))  x -= y;
+            else if (tk.equals("*"))  x *= y;
+            else if (tk.equals("/"))  x /= y;
+            else throw new Exception();
+        }
+        return x;
     }
 }
--- src/test/java/rpn/CLITest.java
+++ src/test/java/rpn/CLITest.java
@@ -3,7 +3,7 @@ package rpn;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static rpn.CLI.evaluate;
+import static rpn.CLI.*;
 
 public class CLITest {
 
@@ -23,7 +23,42 @@ public class CLITest {
     }
 
     @Test
+    public void should_evaluate_simple_substract() {
+        assertThat(evaluate("5 5 -")).isEqualTo(0);
+    }
+
+    @Test
+    public void should_evaluate_simple_division() {
+        assertThat(evaluate("10 2 /")).isEqualTo(5);
+    }
+
+    @Test
+    public void should_evaluate_simple_multiplication() {
+        assertThat(evaluate("10 2 *")).isEqualTo(20);
+    }
+
+    @Test
     public void should_evaluate_more_complex_addition() {
         assertThat(evaluate("2 3 5 + +")).isEqualTo(10);
+    }
+
+    @Test
+    public void should_evaluate_more_more_complex_operation() {
+        assertThat(evaluate("3 5 8 * 7 + *")).isEqualTo(141);
+    }
+
+    @Test
+    public void should_evaluate_more_or_less_complex_operation() {
+        assertThat(evaluate("7 3 + 8 - 2 /")).isEqualTo(1);
+    }
+
+    @Test
+    public void should_evaluate_addition_substraction() {
+        assertThat(evaluate("5 2 3 + -")).isEqualTo(0);
+    }
+
+    @Test
+    public void should_evaluate_addition_substraction_multiplication_division() {
+        assertThat(evaluate("5 2 3 + - 1 + 10 * 2 /")).isEqualTo(5);
     }
 }
